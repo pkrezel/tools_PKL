49af2ea5ff8cb3dbbdd5ac6d0a2d06277995d800
pkrezel <pascal.krezel@gmail.com>
2023-08-18 11:52:16 +0200

    Create pickle_PKL.py
    creation

---
+++ pickle_PKL.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+import logging
+logger = logging.getLogger(__name__)
+# =============================================================================
